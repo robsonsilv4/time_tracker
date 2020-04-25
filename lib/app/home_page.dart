d5e180d56a9016103e6fa97c3b6b861d8858b807
robsonsilv4 <robsonsilv410@gmail.com>
2020-04-25 14:19:22 -0300

    :sparkles: add logout

--- lib/app/home_page.dart
+++ lib/app/home_page.dart
@@ -1,6 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  final VoidCallback onSignOut;
+
+  HomePage({@required this.onSignOut});
+
+  Future<void> _signOut() async {
+    try {
+      await FirebaseAuth.instance.signOut();
+      onSignOut();
+    } catch (error) {
+      print(error.toString());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +30,7 @@ class HomePage extends StatelessWidget {
                 color: Colors.white,
               ),
             ),
-            onPressed: () {},
+            onPressed: _signOut,
           ),
         ],
       ),
