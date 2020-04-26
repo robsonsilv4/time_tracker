475fec9e48765107603dd9c38243ad92b99b384c
robsonsilv4 <robsonsilv410@gmail.com>
2020-04-26 15:13:07 -0300

    :sparkles: add text controllers and toggle

--- lib/app/sign_in/email_sign_in_form.dart
+++ lib/app/sign_in/email_sign_in_form.dart
@@ -2,14 +2,49 @@ import 'package:flutter/material.dart';
 
 import '../../common_widgets/form_submit_button.dart';
 
-class EmailSignInForm extends StatelessWidget {
+enum EmailSignInFormType { signIn, register }
+
+class EmailSignInForm extends StatefulWidget {
+  @override
+  _EmailSignInFormState createState() => _EmailSignInFormState();
+}
+
+class _EmailSignInFormState extends State<EmailSignInForm> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  EmailSignInFormType _formType = EmailSignInFormType.signIn;
+
+  void _submit() {
+    print('Email: ${_emailController.text}');
+    print('Password: ${_passwordController.text}');
+  }
+
+  void _toggleFormType() {
+    setState(() {
+      _formType = _formType == EmailSignInFormType.signIn
+          ? EmailSignInFormType.register
+          : EmailSignInFormType.signIn;
+      _emailController.clear();
+      _passwordController.clear();
+    });
+  }
+
   List<Widget> _buildChildren() {
+    final primaryText = _formType == EmailSignInFormType.signIn
+        ? 'Sign in'
+        : 'Create an account';
+    final secondaryText = _formType == EmailSignInFormType.signIn
+        ? 'Need an account? Register'
+        : 'Have an account? Sign in';
+
     return [
       TextField(
         decoration: InputDecoration(
           labelText: 'Email',
           hintText: 'test@email.cpm',
         ),
+        controller: _emailController,
       ),
       SizedBox(height: 8.0),
       TextField(
@@ -17,16 +52,17 @@ class EmailSignInForm extends StatelessWidget {
           labelText: 'Password',
         ),
         obscureText: true,
+        controller: _passwordController,
       ),
       SizedBox(height: 8.0),
       FormSubmitButton(
-        text: 'Sign in',
-        onPressed: () {},
+        text: primaryText,
+        onPressed: _submit,
       ),
       SizedBox(height: 8.0),
       FlatButton(
-        child: Text('Need an account? Register.'),
-        onPressed: () {},
+        child: Text(secondaryText),
+        onPressed: _toggleFormType,
       ),
     ];
   }
