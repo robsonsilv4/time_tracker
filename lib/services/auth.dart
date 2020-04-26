6d9258b55f2d6f4c8f8ff63eb0eb080577d15011
robsonsilv4 <robsonsilv410@gmail.com>
2020-04-26 12:02:12 -0300

    :sparkles: add login with facebook

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
 class User {
@@ -17,6 +18,8 @@ abstract class AuthBase {
   Future<User> signInAnonymously();
 
   Future<User> signInWithGoogle();
+
+  Future<User> signInWithFacebook();
 
   Future<void> signOut();
 }
@@ -77,9 +80,30 @@ class Auth implements AuthBase {
   }
 
   @override
+  Future<User> signInWithFacebook() async {
+    final facebookLogin = FacebookLogin();
+    final result = await facebookLogin.logIn(['public_profile']);
+    if (result.accessToken != null) {
+      final authResult = await _firebaseAuth.signInWithCredential(
+        FacebookAuthProvider.getCredential(
+          accessToken: result.accessToken.token,
+        ),
+      );
+      return _userFromFirebase(authResult.user);
+    } else {
+      throw PlatformException(
+        code: 'ERROR_ABORTED_BY_USER',
+        message: 'Sign in aborted by user',
+      );
+    }
+  }
+
+  @override
   Future<void> signOut() async {
     final googleSingIn = GoogleSignIn();
     await googleSingIn.signOut();
+    final facebookLogin = FacebookLogin();
+    await facebookLogin.logOut();
     await _firebaseAuth.signOut();
   }
 }
