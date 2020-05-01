58067f7fa2047556ac84b506edd1648158932a15
robsonsilv4 <robsonsilv410@gmail.com>
2020-05-01 16:58:09 -0300

    :sparkles: add api path

---
+++ lib/services/api_path.dart
@@ -0,0 +1,3 @@
+class ApiPath {
+  static String job(String uid, String jobUid) => '/users/$uid/$jobUid';
+}
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
+import 'package:time_tracker/services/api_path.dart';
 
 import '../app/home/models/job.dart';
 
@@ -14,7 +15,7 @@ class FirestoreDatabase implements Database {
 
   @override
   Future<void> createJob(Job job) async {
-    final path = '/users/$uid/jobs/job_abc';
+    final path = ApiPath.job(uid, 'job_abc');
     final documentReference = Firestore.instance.document(path);
     await documentReference.setData(job.toMap());
   }
