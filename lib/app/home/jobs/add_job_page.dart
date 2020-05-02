9fe771323a27bffe80c4ab2437c6ac9adfdc5ceb
robsonsilv4 <robsonsilv410@gmail.com>
2020-05-02 16:40:27 -0300

    :sparkles: add form for new job

--- lib/app/home/jobs/add_job_page.dart
+++ lib/app/home/jobs/add_job_page.dart
@@ -15,6 +15,20 @@ class AddJobPage extends StatefulWidget {
 }
 
 class _AddJobPageState extends State<AddJobPage> {
+  final _formKey = GlobalKey<FormState>();
+
+  String _name;
+  int _ratePerHour;
+
+  bool _validateAndSaveForm() {
+    final form = _formKey.currentState;
+    if (form.validate()) {
+      form.save();
+      return true;
+    }
+    return false;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -23,6 +37,18 @@ class _AddJobPageState extends State<AddJobPage> {
         elevation: 2.0,
         title: Text('New Job'),
         centerTitle: true,
+        actions: <Widget>[
+          FlatButton(
+            onPressed: _submit,
+            child: Text(
+              'Save',
+              style: TextStyle(
+                fontSize: 18.0,
+                color: Colors.white,
+              ),
+            ),
+          )
+        ],
       ),
       body: _buildContents(),
     );
@@ -35,12 +61,48 @@ class _AddJobPageState extends State<AddJobPage> {
         child: Card(
           child: Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Placeholder(
-              fallbackHeight: 200.0,
-            ),
+            child: _buildForm(),
           ),
         ),
       ),
     );
+  }
+
+  Widget _buildForm() {
+    return Form(
+      key: _formKey,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: _buildFormChildren(),
+      ),
+    );
+  }
+
+  List<Widget> _buildFormChildren() {
+    return [
+      TextFormField(
+        decoration: InputDecoration(
+          labelText: 'Job name',
+        ),
+        validator: (value) => value.isNotEmpty ? null : 'Name can\'t be empty.',
+        onSaved: (value) => _name = value,
+      ),
+      TextFormField(
+        decoration: InputDecoration(
+          labelText: 'Rate per hour',
+        ),
+        keyboardType: TextInputType.numberWithOptions(
+          signed: false,
+          decimal: false,
+        ),
+        onSaved: (value) => _ratePerHour = int.tryParse(value) ?? 0,
+      ),
+    ];
+  }
+
+  void _submit() {
+    if (_validateAndSaveForm()) {
+      print('Form saved, name: $_name, ratePerHour: $_ratePerHour');
+    }
   }
 }
